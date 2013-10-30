ee14091d219ae0906d208eab3c84fb75ca9fb116
Federico Tomassetti <f.tomassetti@gmail.com>
2013-10-30 08:55:37 +0100

    Reorganizing

--- app/models.py
+++ app/models.py
@@ -5,16 +5,35 @@ from sqlalchemy.ext.hybrid import hybrid_property, hybrid_method
 ROLE_USER  = 0
 ROLE_ADMIN = 1
 
-class User(db.Model):
+class IdMixin(object):
+    def get_id(self):
+        return self.id
+
+class GetMixin(object):
+    @classmethod
+    def get_all(self):
+        return db.session.query(self).all()
+
+    @classmethod
+    def get_by_id(self, id):
+        return db.session.query(self).filter_by(id=id).first()
+
+tasks_owners = db.Table('tasks_owners', db.Model.metadata,
+    db.Column('task_id', db.Integer,  db.ForeignKey('task.id')),
+    db.Column('owner_id', db.Integer, db.ForeignKey('user.id'))
+)        
+
+class User(db.Model, IdMixin, GetMixin):
     id       = db.Column(db.Integer, primary_key = True)
     name     = db.Column(db.String(128), index = True)
     login    = db.Column(db.String(80), index = True, unique = True)
     email    = db.Column(db.String(120), index = True, unique = True)
     role     = db.Column(db.SmallInteger, default = ROLE_USER)
     password = db.Column(db.String(64))
+    owned_tasks = db.relationship('Task',secondary="tasks_owners",backref='owners')
 
     def is_admin(self):
-    	return self.role==ROLE_ADMIN
+        return self.role==ROLE_ADMIN
 
     # Flask-Login integration
     def is_authenticated(self):
@@ -26,18 +45,6 @@ class User(db.Model):
     def is_anonymous(self):
         return False
 
-    def get_id(self):
-        return self.id
-
-    @classmethod
-    def get_all(self):
-        return db.session.query(self).all()
-
-    @classmethod
-    def get_by_id(self, id):
-        return db.session.query(self).filter_by(id=id).first()
-
-
     # Required for administrative interface
     def __unicode__(self):
         return self.name + "<" + self.email + ">"
@@ -45,14 +52,18 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % (self.email)
 
-class Project(db.Model):
+
+class Task(db.Model, IdMixin, GetMixin):
+    id      = db.Column(db.Integer, primary_key=True)
+    summary = db.Column(db.String(200), unique=True)
+#    owners  = db.relationship('User',secondary="tasks_owners",backref='owned_tasks')
+
+    def __init__(self,summary):
+        self.summary = summary
+
+class Project(db.Model, IdMixin, GetMixin):
     id    = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), unique=True)
-
-    @staticmethod
-    def all():
-        session = Session()
-        return session.query(Project);
 
     @staticmethod
     def exist(_title):
--- templates/projects_list.tpl
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-<ul>
-%for p in projects:
-	<li>{{p.id}} - {{p.title}}</li>
-%end
-</ul>
-</body>
-</html>
